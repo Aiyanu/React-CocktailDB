fb8ba2df662bfa3addb3de695403876d7c1557de
Aiyanu <96719103+Aiyanu@users.noreply.github.com>
2022-12-27 01:06:45 +0100

    Done

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies tellus non sagittis elementum. Curabitur et ipsum velit. Nam varius elit non dui fringilla, et faucibus elit tempus. Cras vel leo velit. Vestibulum finibus, felis a ultricies tempor, odio urna imperdiet urna, id consectetur est elit eu massa. Fusce vestibulum sapien id neque egestas consectetur. 
+      </p>
+    </section>
+  )
+}
+
+export default About
